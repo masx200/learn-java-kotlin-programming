d711f1b50f0df16a4483727aec08f860f2462cc7
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-04 19:05:43 +0800

    FindDirectories

---
+++ src/main/java/chapter/seven/two/one/one/FindDirectories.java
@@ -0,0 +1,25 @@
+package chapter.seven.two.one.one;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FindDirectories {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            args = new String[]{"."};
+        }
+        try {
+            File pathName = new File(args[0]);    //建立文件(目录)对象
+            String[] fileNames = pathName.list();   //路径文件名数组赋值
+            for (String fileName : fileNames) {
+                File f = new File(pathName.getPath(), fileName);
+                if (f.isDirectory()) {
+                    System.out.println(f.getCanonicalPath());      //输出规范文件路径
+                    main(new String[]{f.getPath()});      //遍历所有子目录
+                }
+            }
+        } catch (IOException e) {    //捕捉IO 异常
+            e.printStackTrace();
+        }
+    }
+}
