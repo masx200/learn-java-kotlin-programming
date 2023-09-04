fb48e58001533e5c7fe48627a6cd05560cededd9
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-04 19:36:35 +0800

    if (fileNames != null) {

--- src/main/java/chapter/seven/two/one/one/FindDirectories.java
+++ src/main/java/chapter/seven/two/one/one/FindDirectories.java
@@ -11,11 +11,13 @@ public class FindDirectories {
         try {
             File pathName = new File(args[0]);    //建立文件(目录)对象
             String[] fileNames = pathName.list();   //路径文件名数组赋值
-            for (String fileName : fileNames) {
-                File f = new File(pathName.getPath(), fileName);
-                if (f.isDirectory()) {
-                    System.out.println(f.getCanonicalPath());      //输出规范文件路径
-                    main(new String[]{f.getPath()});      //遍历所有子目录
+            if (fileNames != null) {
+                for (String fileName : fileNames) {
+                    File f = new File(pathName.getPath(), fileName);
+                    if (f.isDirectory()) {
+                        System.out.println(f.getCanonicalPath());      //输出规范文件路径
+                        main(new String[]{f.getPath()});      //遍历所有子目录
+                    }
                 }
             }
         } catch (IOException e) {    //捕捉IO 异常
