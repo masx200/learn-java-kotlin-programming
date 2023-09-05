24703082cf6208a0ee8f64483d2cb21f96058312
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-05 10:07:35 +0800

    Create WelcomeTest.java

---
+++ src/main/java/chapter/one/two/five/one/WelcomeTest.java
@@ -0,0 +1,11 @@
+package chapter.one.two.five.one;
+
+import javax.swing.*;
+
+public class WelcomeTest {                  //定义类
+        public static void main(String[] args) {   //定义main()方法
+                WelcomeFrame frame = new WelcomeFrame(); //创建窗口
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.show();
+        }
+}
