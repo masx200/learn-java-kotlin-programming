b6f62951689828c70af736b3b4dbe95586ef5191
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-08 16:26:34 +0800

    SetExampleTest

---
+++ src/test/java/chapter/eleven/two/one/SetExampleTest.java
@@ -0,0 +1,15 @@
+package chapter.eleven.two.one;
+
+import org.junit.jupiter.api.Test;
+
+import static chapter.eleven.two.one.SetExample.main;
+
+class SetExampleTest {
+
+    @Test
+    void mainTest() {
+
+        main(new String[]{"hello", "world"});
+        main(new String[]{"she", "said","she","is","signorina"});
+    }
+}
