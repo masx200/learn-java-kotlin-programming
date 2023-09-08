c9e8ff515c33aacbb9ae6edb082d7e5272c491bf
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-08 20:59:29 +0800

    MockTest

--- binary-tree-level-order-traversal/src/test/java/org/example/MainTest.java
+++ binary-tree-level-order-traversal/src/test/java/org/example/MainTest.java
@@ -7,20 +7,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 class MainTest {
 
-    @Test
-    void mainTest() {
-        var result = MockTest("3 9 20 null null 15 7 !\n", () -> Main.main(new String[]{}));
-        System.out.println(result);
-    }
-
-    synchronized private static String MockTest(String input, Runnable runnable) {
+    synchronized private static String[] MockTest(String input, Runnable runnable) {
         PrintStream orig = System.out;
 
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         System.setOut(new PrintStream(os, false, StandardCharsets.UTF_8));
+        PrintStream orige = System.err;
+
+        ByteArrayOutputStream ose = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(ose, false, StandardCharsets.UTF_8));
 
         InputStream is = System.in;
         System.setIn(new ByteArrayInputStream(input.getBytes()));
@@ -28,11 +27,18 @@ class MainTest {
             runnable.run();
         } finally {
             System.setOut(orig);
+            System.setErr(orige);
             System.setIn(is);
 
         }
-        return (os.toString(StandardCharsets.UTF_8));
+        return new String[]{(os.toString(StandardCharsets.UTF_8)), (ose.toString(StandardCharsets.UTF_8))};
 
+    }
+
+    @Test
+    void mainTest() {
+        var result = MockTest("3 9 20 null null 15 7 !\n", () -> Main.main(new String[]{}));
+        System.out.println(Arrays.toString(result));
     }
 
 }
