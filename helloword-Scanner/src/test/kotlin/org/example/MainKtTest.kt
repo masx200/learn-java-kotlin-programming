cfccf8e2482661e506b5bd5ee73cc47c6e0ed7a5
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-08 21:21:11 +0800

    Update MainKtTest.kt

--- helloword-Scanner/src/test/kotlin/org/example/MainKtTest.kt
+++ helloword-Scanner/src/test/kotlin/org/example/MainKtTest.kt
@@ -2,12 +2,40 @@ package org.example
 
 import org.junit.jupiter.api.Test
 
+
 class MainKtTest {
 
     @Test
     fun mainTest() {
 
         val result = MockTest("9\n5\n") {
+            main(arrayOf())
+        }
+        println(result.contentToString())
+    }
+
+    @Test
+    fun mainTest2() {
+
+        val result = MockTest("9\n15\n") {
+            main(arrayOf())
+        }
+        println(result.contentToString())
+    }
+
+    @Test
+    fun mainTest3() {
+
+        val result = MockTest("-9\n-5\n") {
+            main(arrayOf())
+        }
+        println(result.contentToString())
+    }
+
+    @Test
+    fun mainTest4() {
+
+        val result = MockTest("-2\n-5\n") {
             main(arrayOf())
         }
         println(result.contentToString())
