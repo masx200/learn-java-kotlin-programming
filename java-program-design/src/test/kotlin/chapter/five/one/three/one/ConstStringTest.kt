e269ddc5764a1087a37de5197abbf6faea6083ab
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-10 18:00:48 +0800

    Create ConstStringTest.kt

---
+++ java-program-design/src/test/kotlin/chapter/five/one/three/one/ConstStringTest.kt
@@ -0,0 +1,14 @@
+package chapter.five.one.three.one
+
+import org.junit.jupiter.api.Test
+
+internal class ConstStringTest {
+    @Test
+    fun testmain() {
+        ConstString.main(
+            arrayOf(
+
+            )
+        )
+    }
+}
