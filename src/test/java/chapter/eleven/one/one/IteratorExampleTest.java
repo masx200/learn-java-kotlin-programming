f828d2232d195f3a3552fb9137c6c216f1b7c091
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-10 16:03:16 +0800

    Create IteratorExampleTest.java

---
+++ src/test/java/chapter/eleven/one/one/IteratorExampleTest.java
@@ -0,0 +1,11 @@
+package chapter.eleven.one.one;
+
+import org.junit.jupiter.api.Test;
+
+class IteratorExampleTest {
+
+    @Test
+    void maintest() {
+        IteratorExample.main(new String[0]);
+    }
+}
