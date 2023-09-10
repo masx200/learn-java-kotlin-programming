84729c69d336148a796724e2c7900ebd5711e995
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-10 19:27:26 +0800

    ForLoopStatementTest

---
+++ java-program-design/src/test/java/chapter/four/three/two/one/DoWhileLoopStatementTest.java
@@ -0,0 +1,10 @@
+package chapter.four.three.two.one;
+
+import org.junit.jupiter.api.Test;
+
+class DoWhileLoopStatementTest {
+    @Test
+    void maintest() {
+        DoWhileLoopStatement.main(new String[]{});
+    }
+}
--- java-program-design/src/test/kotlin/chapter/four/three/three/one/ForLoopStatementTest.kt
+++ java-program-design/src/test/kotlin/chapter/four/three/three/one/ForLoopStatementTest.kt
@@ -1,8 +1,7 @@
 package chapter.four.three.three.one
 
 import org.junit.jupiter.api.Test
-
-internal class ForLoopStatementTest {
+class ForLoopStatementTest {
     @Test
     fun maintest() {
         ForLoopStatement.main(arrayOf())
