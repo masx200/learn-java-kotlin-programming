232a23cdebce17a33a0a5e47972f81426dfa8385
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-10 15:59:51 +0800

    Create FindDirectoriesTest.java

---
+++ src/test/java/chapter/seven/two/two/one/FindDirectoriesTest.java
@@ -0,0 +1,12 @@
+package chapter.seven.two.two.one;
+
+import org.junit.jupiter.api.Test;
+
+class FindDirectoriesTest {
+
+    @Test
+    void mainTest() {
+
+        FindDirectories.main(new String[]{});
+    }
+}
