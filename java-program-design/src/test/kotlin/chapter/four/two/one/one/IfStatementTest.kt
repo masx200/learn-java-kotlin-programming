b640b7da2e5049f712ee173ae14c42fc7017a6a6
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-10 19:51:20 +0800

    test

---
+++ java-program-design/src/test/kotlin/chapter/four/two/one/one/IfStatementTest.kt
@@ -0,0 +1,11 @@
+package chapter.four.two.one.one
+
+import org.junit.jupiter.api.Test
+
+internal class IfStatementTest {
+    @Test
+    fun main() {
+        IfStatement.main(arrayOf())
+    }
+
+}
---
+++ java-program-design/src/test/kotlin/chapter/four/two/one/three/IfElseIfStatementTest.kt
@@ -0,0 +1,10 @@
+package chapter.four.two.one.three
+
+import org.junit.jupiter.api.Test
+
+internal class IfElseIfStatementTest {
+    @Test
+    fun maintest() {
+        IfElseIfStatement.main(arrayOf())
+    }
+}
