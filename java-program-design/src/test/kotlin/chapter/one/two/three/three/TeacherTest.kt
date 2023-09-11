6371e18259797307fd7b7e954876c81d81739fc3
masx200 <34191203+masx200@users.noreply.github.com>
2023-09-11 12:29:31 +0800

    Create TeacherTest.kt

---
+++ java-program-design/src/test/kotlin/chapter/one/two/three/three/TeacherTest.kt
@@ -0,0 +1,11 @@
+package chapter.one.two.three.three
+
+import org.junit.jupiter.api.Test
+
+internal class TeacherTest {
+    @Test
+    fun maintest() {
+        Teacher
+            .main(arrayOf())
+    }
+}
